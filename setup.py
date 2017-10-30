f03adc1ac84d67af4930e1b76278dc3bb63df0c2
aldebaran1 <smrak@bu.edu>
2017-10-30 09:49:05 -0400

    Rinex utils

---
+++ setup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(name='pyRinex',
+      packages=['rinex'],
+      description='Conversion utilities for GPS and GLONASS Rinex files 2.11',
+      author='Sebastijan Mrak',
+      url='https://github.com/aldebaran1/pyRinex',
+	  )
+
